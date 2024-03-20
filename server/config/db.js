67b39267de3d88fe1dd149e3f6e2e2236052fa5b
SachyamDh <iamsaxyamdh@gmail.com>
2024-03-20 06:41:04 +0545

    db config

---
+++ server/config/db.js
@@ -0,0 +1,19 @@
+const postGres = require("sequelize");
+
+const sequelize = new postGres.Sequelize({
+  dialect: "postgres",
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+});
+
+async function dbConnection() {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection Successfull");
+  } catch (error) {
+    console.log("Connection failed");
+  }
+}
--- server/server.js
+++ server/server.js
@@ -1,8 +1,17 @@
 const dotenv = require("dotenv");
+const app = require("./app");
 
 process.on("uncaughtException", (err) => {
   console.log(err.message, err.name);
   console.log(err.stack);
   console.log("Unhandles Exception Error; Shutting down server");
   process.exit(1);
+});
+
+dotenv.config({ path: "../config.env" });
+
+const port = process.env.PORT || 3001;
+
+const server = app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
